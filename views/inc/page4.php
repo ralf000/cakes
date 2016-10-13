9cef8cd306f2e8d6347391761326a189816258b3
ralf <ralf000@yandex.ru>
2016-10-13 18:30:59 +0300

    +вывод отзывов из базы

--- views/inc/page4.php
+++ views/inc/page4.php
@@ -6,101 +6,27 @@
                     <h2 class="heading">Отзывы о моих тортах из подгузников</h2>
                     <div id="reviews">
                         <ul class="bxslider">
-                            <li>
-                                <div class="row" style="padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Евгения Ханина</b></p>
-                                            <p>Марина, это вам огромное спасибо! В восторге все: и коллеги, которые
-                                                принимали участие в выборе подарка, и родители :) Очень-очень красиво!
-                                                Если кто из подруг надумает стать мамой, то я обязательно к вам
-                                                обращусь! ;)</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a class="fancybox" href="/img/news/d4XkwIjP4DM.jpg"><img
-                                                    src="/img/news/d4XkwIjP4DM.jpg"
-                                                    alt="розово-сиреневый торт из подгузников"/></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row" style="border-top: 1px solid #666; padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Morдашка Макарова</b></p>
-                                            <p>Спасибо за такой полезный, красивый подарок!</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a href="/img/news/2.jpg" class="fancybox"><img src="/img/news/2.jpg"
-                                                                                            alt="торт из подгузников для девочки"/></a>
+                            <? if (!empty($this->reviews) && is_array($this->reviews)): ?>
+                                <? foreach ($this->reviews as $key => $review): ?>
+                                    <? if ($key % 2 === 0) echo '<li>'; ?>
+                                    <div class="row" style="padding: 10px;">
+                                        <div class="rev">
+                                            <div class="col-md-8">
+                                                <p class="lead"><b><?= $review['name'] ?></b></p>
+                                                <?= $review['review'] ?>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <a class="fancybox" href="<?= $review['image'] ?>"><img
+                                                        src="<?= $review['image'] ?>"
+                                                        alt="Отзыв в торте из памперсов в Зеленограде"/></a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="row" style="padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Алексей Мамаев</b></p>
-                                            <p>Привет!<br>
-                                                Спасибо, Марина, за поздравление и прекрасный подарок! Чувствуется
-                                                оригинальность подхода, фантазия и доброта исполнения!!!<br>
-                                                Также благодарность от Анны и дочки)</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a href="/img/news/i7-aAIsXtlY.jpg" class="fancybox"><img
-                                                    src="/img/news/i7-aAIsXtlY.jpg"
-                                                    alt="торт из памперсов - обезьяна-велосипедистка"/></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row" style="border-top: 1px solid #666; padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Алена Аксенова</b></p>
-                                            <p>Марина, хочу еще раз выразить вам огромную благодарность за то чудо,
-                                                которое вы для нас сотворили!!! Готовый тортик полность совпал с моим
-                                                представлением о нем...<br>
-                                                Многие из знакомых в первый момент даже не поняли что это тортик из
-                                                памперсов...</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a href="/img/news/bW94knOqCFM.jpg" class="fancybox"><img
-                                                    src="/img/news/bW94knOqCFM.jpg"
-                                                    alt="торт из подгузников из материалов заказчика"/></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="row" style="padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Олеся Дербина</b></p>
-                                            <p>Марина, Большое спасибо, отличный подарок, очень понравился молодой
-                                                мамочке)</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a href="/img/news/MgUwCgAr370.jpg" class="fancybox"><img
-                                                    src="/img/news/MgUwCgAr370.jpg"
-                                                    alt="торт из памперсов для мальчика в Зеленограде"/></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row" style="border-top: 1px solid #666; padding: 10px;">
-                                    <div class="rev">
-                                        <div class="col-md-8">
-                                            <p class="lead"><b>Ольга Тамонина</b></p>
-                                            <p>Наш тортик! Очень красивый и полезный! Был...))) Марина,сделай нам еще
-                                                такой) На 2 годика)</p>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <a href="/img/news/k7SuS5Mbkls.jpg" class="fancybox"><img
-                                                    src="/img/news/k7SuS5Mbkls.jpg"
-                                                    alt="большой торт из памперсов"/></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </li>
+                                    <? if ($key % 2 === 1) echo '</li>'; ?>
+                                <? endforeach; ?>
+                            <? else: ?>
+                                <p>Отзывов пока нет</p>
+                            <? endif; ?>
                         </ul>
                     </div>
                 </div>
