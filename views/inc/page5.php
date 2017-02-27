2a627b0d86d30631ef051ba5033794c204a3ac75
ralf <ralf000@yandex.ru>
2017-02-27 16:47:06 +0300

    misc

--- views/inc/page5.php
+++ views/inc/page5.php
@@ -6,36 +6,35 @@
                     <h2 class="heading">Новости</h2>
                     <div class="row">
                         <? if (!empty($this->news) && is_array($this->news)): ?>
-                            <? $cnt = 0 ?>
                             <div id="news">
                                 <ul class="bxslider">
-                                    <li>
-                                        <? foreach ($this->news as $n): ?>
-                                            <div class="wrapper_body">
-                                                <div class="cbm_wrap ">
-                                                    <h1><?= $n['title'] ?></h1>
-<!--                                                    <small>-->
-<!--                                                        Добавлено: --><?//= date('H:i:s d-m-Y', strtotime($n['created_time'])) ?><!--</small>-->
-                                                    <? if (isset($n['image']) && !empty($n['image'])): ?>
-                                                        <div class="newsImg">
-                                                            <a class="fancybox" href="<?= $n['image'] ?>"><img
-                                                                    src="<?= $n['image'] ?>"
-                                                                    class="img-responsive center-block"></a>
-                                                        </div>
-                                                    <? endif; ?>
-                                                    <? if (isset($n['description']) && !empty($n['description'])): ?>
-                                                        <div class="description"><p><?= $n['description'] ?></p></div>
-                                                    <? endif; ?>
-                                                    <a href="<?= $n['id'] ?>" class="viewnews btn btn-default"
-                                                       data-toggle="modal" data-target="#view">Читать далее</a>
+                                <?php $newsGroups = array_chunk($this->news, 3); ?>
+                                    <? foreach ($newsGroups as $newsGroup): ?>
+                                        <li>
+                                            <?php foreach ($newsGroup as $n): ?>
+                                                <div class="wrapper_body">
+                                                    <div class="cbm_wrap ">
+                                                        <h3><?= $n['title'] ?></h3>
+                                                        <!-- <small>-->
+                                                        <!-- Добавлено: -->
+                                                        <? //= date('H:i:s d-m-Y', strtotime($n['created_time'])) ?><!--</small>-->
+                                                        <? if (isset($n['image']) && !empty($n['image'])): ?>
+                                                            <div class="newsImg">
+                                                                <a class="fancybox" href="<?= $n['image'] ?>"><img
+                                                                        src="<?= $n['image'] ?>"
+                                                                        class="img-responsive center-block"></a>
+                                                            </div>
+                                                        <? endif; ?>
+                                                        <? if (isset($n['description']) && !empty($n['description'])): ?>
+                                                            <div class="description"><p><?= $n['description'] ?></p></div>
+                                                        <? endif; ?>
+                                                        <a href="<?= $n['id'] ?>" class="viewnews btn btn-default"
+                                                           data-toggle="modal" data-target="#view">Читать далее</a>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <?
-                                            $cnt++;
-                                            if ($cnt === 3)
-                                                echo '</li><li>';
-                                            ?>
-                                        <? endforeach; ?>
+                                            <?php endforeach; ?>
+                                        </li>
+                                    <? endforeach; ?>
                                 </ul>
                             </div>
                         <? else: ?>
